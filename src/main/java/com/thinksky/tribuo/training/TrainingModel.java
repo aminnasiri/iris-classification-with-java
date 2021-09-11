e606d97e948fe4cc916d2ecd1aa026d48cbc2ba0
Think Sky <admin@thinksky.com>
2021-09-10 22:19:22 -0700

    Upgraded test code structure.

--- src/main/java/com/thinksky/tribuo/training/TrainingModel.java
+++ src/main/java/com/thinksky/tribuo/training/TrainingModel.java
@@ -8,31 +8,32 @@ import org.tribuo.MutableDataset;
 import org.tribuo.Trainer;
 import org.tribuo.classification.Label;
 import org.tribuo.classification.LabelFactory;
+import org.tribuo.classification.evaluation.LabelEvaluation;
 import org.tribuo.classification.evaluation.LabelEvaluator;
 import org.tribuo.classification.sgd.linear.LogisticRegressionTrainer;
 import org.tribuo.data.csv.CSVLoader;
 import org.tribuo.evaluation.TrainTestSplitter;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.net.URL;
 
-public class Application {
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+public class TrainingModel {
+    private static final Logger logger = LoggerFactory.getLogger(TrainingModel.class);
 
-    public static void main(String... args) throws IOException {
+    private MutableDataset<Label> testingDataset;
+
+    public Model<Label> train(URL url) throws IOException {
         logger.info("Application starts");
-
 
         var labelFactory = new LabelFactory();
         var csvLoader = new CSVLoader<>(labelFactory);
 
         var irisHeaders = new String[]{"sepalLength", "sepalWidth", "petalLength", "petalWidth", "species"};
-        var irisesSource = csvLoader.loadDataSource(ClassLoader.getSystemResource("bezdekIris.data"), "species", irisHeaders);
+        var irisesSource = csvLoader.loadDataSource(url, "species", irisHeaders);
         var irisSplitter = new TrainTestSplitter<>(irisesSource, 0.7, 1L);
 
         var trainingDataset = new MutableDataset<>(irisSplitter.getTrain());
-        var testingDataset = new MutableDataset<>(irisSplitter.getTest());
+        testingDataset = new MutableDataset<>(irisSplitter.getTest());
 
         System.out.printf("Training data size = %d, number of features = %d, number of classes = %d%n", trainingDataset.size(), trainingDataset.getFeatureMap().size(), trainingDataset.getOutputInfo().size());
 
@@ -42,30 +43,14 @@ public class Application {
         Trainer<Label> trainer = new LogisticRegressionTrainer();
         logger.info(trainer.toString());
 
-        Model<Label> irisModel = trainer.train(trainingDataset);
+        return trainer.train(trainingDataset);
+    }
+
+    public LabelEvaluation evaluator(Model<Label> model){
 
         var evaluator = new LabelEvaluator();
-        var evaluation = evaluator.evaluate(irisModel, testingDataset);
-        logger.info(evaluation.toString());
-
-        logger.info(evaluation.getConfusionMatrix().toString());
-
-        logger.info("\n\n=====================Feature=========================\n");
-
-        var featureMap = irisModel.getFeatureIDMap();
-        for (var v : featureMap) {
-            System.out.println(v.toString());
-            System.out.println();
-        }
-
-
-        logger.info("\n\n=================Save Model==========================\n");
-        var writeOutputStream = new FileOutputStream("irisModel.bin");
-        var objectOutputStream = new ObjectOutputStream(writeOutputStream);
-        objectOutputStream.writeObject(irisModel);
-        objectOutputStream.flush();
-        objectOutputStream.close();
-
-        logger.info("Application ends training");
+        return evaluator.evaluate(model, testingDataset);
     }
+
+
 }
--- src/test/java/com/thinksky/tribuo/training/ApplicationTest.java
+++ src/test/java/com/thinksky/tribuo/training/ApplicationTest.java
@@ -1,5 +1,50 @@
 package com.thinksky.tribuo.training;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tribuo.classification.evaluation.LabelEvaluation;
+import org.tribuo.classification.evaluation.LabelEvaluator;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 class ApplicationTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationTest.class);
+
+    @Test
+    void addition() throws Exception {
+
+        var trainingModel = new TrainingModel();
+        var train = trainingModel.train(ClassLoader.getSystemResource("bezdekIris.data"));
+        var evaluator = trainingModel.evaluator(train);
+
+        logger.info(evaluator.toString());
+
+        logger.info(evaluator.getConfusionMatrix().toString());
+
+        logger.info("\n\n=====================Feature=========================\n");
+
+        var featureMap = train.getFeatureIDMap();
+        for (var v : featureMap) {
+            System.out.println(v.toString());
+            System.out.println();
+        }
+
+        assert evaluator.accuracy() > 0.9;
+        Assertions.assertTrue(evaluator.accuracy() > 0.9);
+
+        logger.info("\n\n=================Save Model==========================\n");
+
+        var writeOutputStream = new FileOutputStream("irisModel.bin");
+        var objectOutputStream = new ObjectOutputStream(writeOutputStream);
+        objectOutputStream.writeObject(train);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        logger.info("Application ends training");
+    }
 
 }
